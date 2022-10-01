ffc6dca1a6d8c60c2dc571b58292985991bdb62a
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2022-10-02 02:51:22 +0530

    Added modal to confirm to delete image

--- src/components/Image.js
+++ src/components/Image.js
@@ -42,12 +42,48 @@ function Image({ setSelectedImg, user }) {
             />
             <button
               className="image-delete"
-              onClick={() => {
-                deleteImage(index);
-              }}
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
             >
               <i className="fas fa-trash-alt trash"></i>
             </button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Confirm deletion</h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <p>Do you confirm to delete this image?</p>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      data-bs-dismiss="modal"
+                      onClick={() => {
+                        deleteImage(index);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </motion.div>
         ))}
     </div>
