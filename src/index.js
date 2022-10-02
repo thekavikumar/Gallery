a0ac7a70c941a48e0a5ef10b3cfad1a61177e933
Theodore Kluge <tkluge@fmm.com>
2022-10-02 15:22:13 -0400

    move provider to index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from './providers/ThemeProvider';
+import { UserProvider } from './providers/UserProvider';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ThemeProvider>
-    <App />{" "}
+    <UserProvider>
+      <App />
+    </UserProvider>
   </ThemeProvider>
 );
