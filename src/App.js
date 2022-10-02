e34ad6c84f46e8fa3e78b2360b5d646ee5f61e42
Theodore Kluge <tkluge@fmm.com>
2022-10-02 15:22:13 -0400

    move provider to index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import { UserProvider } from './providers/UserProvider';
 
 function App () {
   return (
     <BrowserRouter>
-      <UserProvider>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </UserProvider>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import ThemeContextWrapper from "./ThemeContextWrapper.js";
+import { UserProvider } from './providers/UserProvider';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ThemeContextWrapper>
-    <App />{" "}
+    <UserProvider>
+      <App />
+    </UserProvider>
   </ThemeContextWrapper>
 );
